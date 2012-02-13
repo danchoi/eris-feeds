caaae30cb52cb727434f6c179dc507ee6efa5f1d
Daniel Choi <dhchoi@gmail.com>
2012-02-13 12:06:18 -0500

    update wc

--- lib/update_word_counts.rb
+++ lib/update_word_counts.rb
@@ -6,4 +6,5 @@ DB[:items].each do |item|
   wc = d.inner_text.split(/\s+/).size
   item[:word_count] = wc
   puts "%s => %d" % [item[:title], wc]
+  DB[:items].filter(item_id:item[:item_id]).update(word_count:wc)
 end
